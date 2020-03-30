4254ebb2aa69d53157b8941301bc1af9a270967c
a1824jj <a1824jj@aiit.ac.jp>
2020-03-30 12:47:53 +0900

    make project & add Capability Near Field Communication

---
+++ NFCReader-sampel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NFCReader-sampel
+//
+//  Created by JONGWOO JIN on 2020/03/30.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

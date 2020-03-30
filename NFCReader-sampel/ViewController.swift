bb1bf54e75c56bd413ce712829b892fb425112c8
a1824jj <a1824jj@aiit.ac.jp>
2020-03-30 13:39:40 +0900

    read tag

--- NFCReader-sampel/ViewController.swift
+++ NFCReader-sampel/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var DataView: UITextField!
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
     
+    @IBOutlet weak var DataView: UITextField!
+       
     @IBAction func startReadNFC(_ sender: Any) {
-        
-        
+           let session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+           session.begin()
+           print("セッションをスタートしました")
     }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error as Any)
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        print(messages as Any)
+        for message in messages {
+            for payload in message.records {
+                if let payloadString = String.init(data: payload.payload.advanced(by: 1), encoding: String.Encoding.utf8){
+                    print("payloadString")
+                    self.DataView.text = payloadString
+                }
+            }
+        }
+    }
+    
+
+   
     
     
     override func viewDidLoad() {
